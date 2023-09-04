cf83511df3b51130011f0203426a45c2330b4d8d
Christian Gitter <c.gitter@heurich.de>
2023-09-04 09:31:32 +0200

    add .env

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 1. express -> `npm i express`
 2. body-parser -> `npm i body-parser`
+3. .env -> `npm i dotenv`
+
+
--- api.js
+++ api.js
@@ -6,20 +6,18 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 // Define a POST route that expects a 'password' key in the request body
-app.post('/login', (req, res) => {
+app.post('/', (req, res) => {
+
     // Check if the required 'password' key exists in the request body
-    if (!req.body.hasOwnProperty('password')) {
-        return res.status(400).json({ error: 'The "password" key is required in the request body' });
+    if (!req.body.hasOwnProperty('key')) {
+        return res.status(400).json({ error: 'The key is required in the request body' });
     }
 
     // Access the 'password' key in the request body
     const password = req.body.password;
-    console.log(`The password sent is "${password}"`);
+    console.log(`The key sent is "${password}"`);
 
     // Now you can validate or use the password as needed
     // Replace this with your actual password validation logic
